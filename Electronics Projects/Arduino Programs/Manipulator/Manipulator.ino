d28e845a141c4801ebcc91068927a29e1b1063f2
Pavel <36619129+pavtiger@users.noreply.github.com>
2019-02-09 17:00:00 +0300

    Made for Arduino
    Pavel A

---
+++ Electronics Projects/Arduino Programs/Manipulator/Manipulator.ino
@@ -0,0 +1,53 @@
+#include <Servo.h>
+
+#define LeftSenter 10
+#define RightSenter 160
+#define HandEnd 125
+
+Servo Hand;
+Servo Left;
+Servo Right;
+String State; //move, stuck, grab, need help
+int functions[2];
+
+void setup() {
+  functions = (
+    "capture",
+    "release"
+  )
+  
+  delay(1000);
+  Serial.begin(9600);
+  Serial.println("BEGIN");
+  Serial.println("Print 'Help' for all commands");
+  
+  Hand.attach(3);
+  Left.attach(4);
+  Right.attach(5);
+  pinMode(6, INPUT);
+  
+  Left.write(LeftSenter);
+  Right.write(RightSenter);
+  Hand.write(HandEnd);
+}
+
+void loop() {
+  String str = Serial.readString();
+
+  if (str != "") {
+    Serial.println(str);
+  }
+  if (str == "help") {
+    for() {
+      Serial.println();
+    }
+    //Serial.println("  capture");
+    //Serial.println("  release");
+  }
+  if (str == "capture") {
+    Hand.write(10);
+  }
+  if (str == "release") {
+    Hand.write(HandEnd);
+  }
+}
